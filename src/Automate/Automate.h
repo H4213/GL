12ab3e2c40857fb9e081a1c37d4b36ef366e1eeb
Toumassa <artogu@live.fr>
2015-03-04 21:31:53 +0100

    Décalage et réduction implémentés
    Il reste à définir les transitions pour chaque état.

--- src/Automate/Automate.h
+++ src/Automate/Automate.h
@@ -1,18 +1,27 @@
 #ifndef AUTOMATE_H
 #define AUTOMATE_H
-
+#include <deque>
+#include <vector>
 #include "Symbole.h"
 #include "Etat.h"
 
-class Etat;
+using namespace std;
 
+class Etat;
 class Automate
 {
 	public:
 		Automate(){}
 		virtual ~Automate(){}
 		
-		void decalage(Symbole s, Etat*);
+		void decalage(Symbole *s, Etat*);
+		void reduction(Symbole *s, Etat*, int);
+		void accepter();
+		void erreur();
+		void lecture();
+	private:
+		deque<Symbole*> _pileSymboles;
+		deque<Etat*> _pileEtats;
 };
 
 #endif
--- src/Automate/Etat.cpp
+++ src/Automate/Etat.cpp
@@ -4,195 +4,234 @@
 
 
 
-bool E0::transition(Automate & automate, Symbole *s)
+void E0::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E1::transition(Automate & automate, Symbole *s)
+void E1::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E2::transition(Automate & automate, Symbole *s)
+void E2::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E3::transition(Automate & automate, Symbole *s)
+void E3::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E4::transition(Automate & automate, Symbole *s)
+void E4::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E5::transition(Automate & automate, Symbole *s)
+void E5::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E6::transition(Automate & automate, Symbole *s)
+void E6::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E7::transition(Automate & automate, Symbole *s)
+void E7::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E8::transition(Automate & automate, Symbole *s)
+void E8::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E9::transition(Automate & automate, Symbole *s)
+void E9::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
-bool E10::transition(Automate & automate, Symbole *s)
+void E10::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
-bool E11::transition(Automate & automate, Symbole *s)
+void E11::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E12::transition(Automate & automate, Symbole *s)
+void E12::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E13::transition(Automate & automate, Symbole *s)
+void E13::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E14::transition(Automate & automate, Symbole *s)
+void E14::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E15::transition(Automate & automate, Symbole *s)
+void E15::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E16::transition(Automate & automate, Symbole *s)
+void E16::transition(Automate & automate, Symbole *s)
 {
-	return 1;
-}
-bool E17::transition(Automate & automate, Symbole *s)
-{
-	return 1;
-}
+	
 
-bool E18::transition(Automate & automate, Symbole *s)
+}
+void E17::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E19::transition(Automate & automate, Symbole *s)
+void E18::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E20::transition(Automate & automate, Symbole *s)
+void E19::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E21::transition(Automate & automate, Symbole *s)
+void E20::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E22::transition(Automate & automate, Symbole *s)
+void E21::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E23::transition(Automate & automate, Symbole *s)
+void E22::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E24::transition(Automate & automate, Symbole *s)
+void E23::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E25::transition(Automate & automate, Symbole *s)
+void E24::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E26::transition(Automate & automate, Symbole *s)
+void E25::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E27::transition(Automate & automate, Symbole *s)
+void E26::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E29::transition(Automate & automate, Symbole *s)
+void E27::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E30::transition(Automate & automate, Symbole *s)
+void E29::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E31::transition(Automate & automate, Symbole *s)
+void E30::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E32::transition(Automate & automate, Symbole *s)
+void E31::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E33::transition(Automate & automate, Symbole *s)
+void E32::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E34::transition(Automate & automate, Symbole *s)
+void E33::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E35::transition(Automate & automate, Symbole *s)
+void E34::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E36::transition(Automate & automate, Symbole *s)
+void E35::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E37::transition(Automate & automate, Symbole *s)
+void E36::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
 
-bool E38::transition(Automate & automate, Symbole *s)
+void E37::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
+}
+
+void E38::transition(Automate & automate, Symbole *s)
+{
+	
+
 }
 
 
-bool E39::transition(Automate & automate, Symbole *s)
+void E39::transition(Automate & automate, Symbole *s)
 {
-	return 1;
+	
+
 }
